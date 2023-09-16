7c3f07bfc2efe3b1e3b6b4c1a0d84c53c9640712
Sunil@github <40825078+Sunilnishad@users.noreply.github.com>
2023-09-16 13:33:18 +0530

    all files

---
+++ backend_springBoot/src/test/java/com/sunil/emp/EmpManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
